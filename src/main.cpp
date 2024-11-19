1d96cc6a6aed293bfacd3ad56b7a3b94dd83db37
Albin Horlaville <98279992+AlbinHorlaville@users.noreply.github.com>
2024-11-19 12:53:45 -0500

    oui

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include "ofMain.h"
 #include "ofApp.h"
-
+#include "../TestMatrix3.h"
+#include "../TestMatrix4.h"
+#include "../TestQuaternion.h"
 
 //========================================================================
 int main( ){
+
+	// Tests Matrix3, Matrix4 and Quaternion
+	TestMatrix3 testMatrix3;
+	testMatrix3.runTestMatrix3();
+	TestMatrix4 testMatrix4;
+	testMatrix4.runTestMatrix4();
+	TestQuaternion testQuaternion;
+	testQuaternion.runTests();
 
 	//Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
 	ofGLWindowSettings settings;
